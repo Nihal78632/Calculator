7f124047a062371c9c6513555019f2285753e784
Ahamed Nihal <nidt786@gmail.com>
2023-02-02 15:10:01 +0530

    calculator web page

---
+++ calc.js
@@ -0,0 +1,13 @@
+function dispplaycontent(content){
+    result.value+=content;
+
+}
+function allclear(){
+    result.value=""
+}
+function erasedd(){
+  result.value=result.value.slice(0, -1);
+}
+function evalu(){
+    result.value=eval(result.value)
+}
